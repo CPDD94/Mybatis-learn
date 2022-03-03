67b916e2ac73a241de614bcc33bd2686c43e150b
ChenHaHa <462433864@qq.com>
2022-03-03 14:37:41 +0800

    mybatis-01

--- mybatis-01/src/main/java/com/example/dao/UserMapper.java
+++ mybatis-01/src/main/java/com/example/dao/UserMapper.java
@@ -3,7 +3,19 @@ package com.example.dao;
 import com.example.pojo.User;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
+    //查询全部用户
     List<User> getUserList();
+    //根据id查询用户
+    User getUserById(int id);
+    //插入用户
+    int addUser(User user);
+    //更新用户
+    int updateUser(User user);
+    //删除用户
+    int deleteUser(int id);
+    //新增用户2
+    int addUser2(Map map);
 }
--- mybatis-01/src/test/java/MyTest.java
+++ mybatis-01/src/test/java/MyTest.java
@@ -1,20 +1,40 @@
 import com.example.dao.UserMapper;
-import com.example.pojo.User;
 import com.example.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
-import java.util.List;
+import java.util.HashMap;
 
 public class MyTest {
     @Test
     public void test() {
         SqlSession session = MybatisUtils.getSqlSession();
         UserMapper mapper = session.getMapper(UserMapper.class);
-        List<User> userList = mapper.getUserList();
+        // 查询用户list
+        /*List<User> userList = mapper.getUserList();
         for (User user : userList) {
             System.out.println(user);
-        }
+        }*/
+        // 根据id查询用户
+        /*User user = mapper.getUserById(1);
+        System.out.println(user);*/
+        // 新增user
+        /*User user = new User();
+        user.setId(4);
+        user.setName("法外狂徒");
+        user.setPwd("123456");
+        mapper.addUser(user);
+        session.commit();*/
+        //修改用户
+        /*User user = new User(3, "李逵", "888888");
+        mapper.updateUser(user);*/
+        // 新增用户2
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("id",5);
+        map.put("name","cccp");
+        map.put("pwd","666666");
+        mapper.addUser2(map);
+
         session.close();
     }
 }
